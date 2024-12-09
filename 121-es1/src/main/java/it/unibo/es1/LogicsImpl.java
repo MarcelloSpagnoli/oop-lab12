df6b9da3a60621f379a9cdc94b455ead3fe891d1
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2024-12-09 16:38:11 +0100

    121 checked by tutor this morning

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,58 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.Collections;
 import java.util.List;
 
 public class LogicsImpl implements Logics {
 
+	// private Integer size; 
+	private List<Integer> values;
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		// this.size = size; --> inferable from values.size
+		this.values = new ArrayList<>(Collections.nCopies(size, 0));
+		// for (int i = 0; i < size; i++) {
+		// 	values.add(0);
+		// }
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.values.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return values;
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return values().stream()
+			.map(x -> x < this.values.size())
+			.toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		Integer i = values.get(elem);
+		i++;
+		values.set(elem, i);
+		return i;
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return values.stream()
+			.map(x -> x.toString())
+			.collect(Collectors.joining("|", "<<", ">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return values.stream()
+			.allMatch(x -> x == values.getFirst());
 	}
 }
